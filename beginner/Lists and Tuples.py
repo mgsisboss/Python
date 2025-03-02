a4d4171162316a82c53113956b1d21e21fec71f8
mgsisboss <mgsisboss@gmail.com>
2025-03-02 18:15:42 -0500

    Update Lists and Tuples

---
+++ beginner/Lists and Tuples.py
@@ -0,0 +1,30 @@
+# Lists and Tuples
+
+# List is another Data Type  example    Fruits = [ ]
+
+#fruits = ["apple", "Pear", 3]   # 2 strings and 1 integer in this list
+#print(fruits)  # prints  ["apple", "Pear", 3]
+
+#fruits = ["apple", "Pear", 3]   # 2 strings and 1 integer in this list
+#print(fruits[1]) #indices are created when you nake a list, starts with "0" so apple is indices 0, pear is indices 1, integer 3 is indices 3,   so "1" prints "pear"
+
+#add to list
+
+#fruits = ["apple", "Pear"]
+#print(fruits)  #prints apple and pear
+#fruits.append("strawberry") # adds strawberry to list
+#print(fruits) # prints all three fruits "apple, pear, strawberry
+#fruits.append("blueberry") # adds blueberry to the list
+#print(fruits) # prints all four fruits "apple, pear, strawberry, blueberry"
+##
+
+#change something in list
+
+#fruits = ["apple", "Pear", "strawberry"]
+#fruits[1] = "blueberry"     #to change, you want to add square bracket and indices, for example we will use "pear" so "1" and "=" the fruit we want so "blueberry"
+#print(fruits) #new print will be "apple, blueberry, strawberry"
+
+#Tupil- another data type used for coordinates and rectangles and colors example below
+#position = (2, 3)
+#color = (255, 255, 255)
+#print(type(color)) #prints <class 'tuple'>
