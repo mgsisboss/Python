1efa19b8e9fb7b8101a614d4d50b039f805b38fc
mgsisboss <mgsisboss@gmail.com>
2025-03-02 18:39:43 -0500

    For loops advanced with Lists

---
+++ beginner/For loops advanced with Lists.py
@@ -0,0 +1,15 @@
+# For loops advanced with Lists
+
+# fruits = ["apple", "pears", "strawberrys"]
+# for fruit in fruits:  #this iterates by item "fruits" while in range iterates by indices/index
+ #   print(fruit)  #prints all items in the list and looping each fruit
+
+
+fruits = ["apples", "pears", "strawberrys", "peachs", "blackberrys", "Oranges"]
+
+#for fruit in fruits:
+#    if fruit == "pears":
+#     print(fruit)
+#    else:
+#        print ("not pears")   # Prints "not pears, Pears, not pears, not pears, not pears, not pears
+                              # this is because we asked it to find the fruit "pears" and if it finds pears it is equal to fruit, if not equal to fruit it will display not pears, so we have one "pears" in the list and 5 "not pears" in the list
