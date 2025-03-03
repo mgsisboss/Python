27e3eac084485aefeeafa626d3215227af796858
mgsisboss <mgsisboss@gmail.com>
2025-03-03 11:14:16 -0500

    Functions

---
+++ beginner/Functions.py
@@ -0,0 +1,25 @@
+#Functions
+
+#def addTwo (x):  #name of function is definition "def" then name of function "addTwo"  than a parameter "x" can be anything, then "return x + 2"
+    #return x + 2 #example of a function
+
+#def subtractTwo(number): #name of function is definition "def" then name of function "subtractTwo"  than a parameter "number" then "return"  "number - 2"
+#    return number - 2
+
+### Testing Function
+#newNumber = addTwo(8)  #need to declare a variable to run the function in this case it can be anything but I named it "newNumber"
+#print(newNumber)  #Prints 10 because 8 + 2 (addTwo) = 10
+#newNumber = subtractTwo(8)
+#print(newNumber) # #Prints 6 because 8 - 2 (subtractTwo) = 6
+
+#def printString(string):  #this finction "printstring" allows us to print a string
+#    print(string)
+
+#printString("hello")  #prints "hello" from the function
+#                      # #keynote arguments is the green "hello" parameter is what we input for the defined function so "string" in line 15
+
+
+
+
+
+
